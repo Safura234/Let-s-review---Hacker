6366205624f52d5728a954ff48996a6323bddb55
Safura234 <safuramg@code.edu.az>
2022-05-18 16:05:43 +0400

    https://github.com/Safura234/Let-s-review---Hacker.git

---
+++ Day 6 Let's review/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_6_Let_s_review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
